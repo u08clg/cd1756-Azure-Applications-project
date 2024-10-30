c655c0108b34595f293fe5ec2deb439a32b68e5f
u08clg <claire.geddes@chevron.com>
2024-10-30 20:50:32 +0000

    Update __init__.py

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 ######## added below section #############
 streamHandler = logging.StreamHandler()
-streamHandler.setLevel(logging.INFO)
+streamHandler.setLevel(logging.WARNING)
 app.logger.addHandler(streamHandler)
 ##########################################
 Session(app)
